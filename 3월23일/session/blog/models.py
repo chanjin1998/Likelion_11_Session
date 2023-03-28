d5086e5dd80081a324910c3d755971598381d7ec
chanjin1998 <chanjin@kookmin.ac.kr>
2023-03-28 18:17:19 +0900

    feat: django models RC

--- 3월23일/session/blog/models.py
+++ 3월23일/session/blog/models.py
@@ -6,9 +6,12 @@ from django.db import models
 class Blog(models.Model):
   title = models.CharField(max_length=100)
   created_at = models.DateTimeField(auto_now_add=True)
-  introduce = models.TextField()
+  content = models.TextField()
   email = models.EmailField(max_length=100, null=True)
   upload = models.FileField(upload_to='uploads/',null=True)
 
   def __str__(self):
     return self.title
+
+  def summary(self):
+    return self.content[:100]
--- 3월23일/session/blog/views.py
+++ 3월23일/session/blog/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404,redirect
 # Create your views here.
 from .models import Blog
 from django.http import HttpResponse
 
-def blog(request):
-  blog = Blog.objects.all()
-  return HttpResponse(blog)
+def home(request):
+  blogs = Blog.objects.all() #Blog에 있는 객체 전부 가져오기
+  return render(request,'home.html',{'blogs':blogs})
+
+def detail(request,blog_id):
+  blog = get_object_or_404(Blog, pk=blog_id)
+  return render(request,'detail.html',{'blog':blog})
+
+#뒤에 블로그가 위에 블로그 앞에 블로그가 html 블로그
+def new(request):
+  return render(request,'new.html')
+
+def create(request):
+  new_blog = Blog()
+  new_blog.title = request.POST['title']
+  new_blog.content = request.POST['content']
+  new_blog.save()
+  return redirect('detail',new_blog.id)
